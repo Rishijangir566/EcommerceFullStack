71037020accfba59546cf51f7aefb7c5bbb0541f
Rishijangir566 <rishijangirjr@gmail.com>
2025-04-24 16:10:38 +0530

    DEBUGGING added options

--- Front/src/Pages/Login.jsx
+++ Front/src/Pages/Login.jsx
@@ -1,82 +1,91 @@
-import { useState } from "react"
-import { Link, useNavigate } from "react-router-dom"
-import instance from "../axiosConfig"
-import { useAuth } from "../context/AuthProvider"
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import instance from "../axiosConfig";
+import { useAuth } from "../context/AuthProvider";
 
 function Login() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const [data, setData] = useState({
-        email: "",
-        password: ""
-    })
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
 
-    const { checkAuth } = useAuth();
+  const { checkAuth } = useAuth();
 
-    function handlechange(e) {
-        const { name, value } = e.target
-        setData((prev) => {
-            return { ...prev, [name]: value }
-        })
-    }
+  function handlechange(e) {
+    const { name, value } = e.target;
+    setData((prev) => {
+      return { ...prev, [name]: value };
+    });
+  }
 
-    async function handleSubmit(e) {
-        e.preventDefault()
-        try {
-            const response = await instance.post("/user/login", data, { withCredentials: true })
-            console.log(response.data);
-            checkAuth()
-            if (response.status === 200 && response.data.message === "Login Successful") {
-
-                const searchParams = new URLSearchParams(window.location.search)
-                const URLParams = searchParams.get("referer")
-                if (URLParams) {
-                    window.location.href = URLParams;
-                }
-                else {
-                    navigate("/")
-                }
-
-            }
-
-            // window.location.href("/")
-        } catch (error) {
-            console.log(error);
-
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      const response = await instance.post("/user/login", data, {
+        withCredentials: true,
+      });
+      console.log(response.data);
+      checkAuth();
+      if (
+        response.status === 200 &&
+        response.data.message === "Login Successful"
+      ) {
+        const searchParams = new URLSearchParams(window.location.search);
+        const URLParams = searchParams.get("referer");
+        if (URLParams) {
+          window.location.href = URLParams;
+        } else {
+          navigate("/");
         }
+      }
+
+      // window.location.href("/")
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    return (
-        <>
-            <div className=" py-4 px-8 bg-red-100 rounded-2xl w-[30%]  mx-auto  my-20 ">
-
-
-                <h2 className="text-center my-4 text-3xl font-medium"> Login Form</h2>
-                <form action="" onSubmit={handleSubmit} className="flex-col flex">
-                    <input
-                        type="email"
-                        placeholder="Enter Email"
-                        name="email"
-                        value={data.email}
-                        onChange={handlechange}
-                        className="border mt-4 pl-2"
-                    />
-                    <input
-                        type="password"
-                        placeholder="Enter Password"
-                        name="password"
-                        value={data.password}
-                        onChange={handlechange}
-                        className="border mt-4 pl-2"
-                    />
-                    <button type="submit" className="bg-green-300 py-1 my-8 rounded text-xl font-bold"> Login</button>
-                </form>
-            </div>
-            <p className="my-8">
-                New User ? <Link to="/user/register" className="text-blue-700 "> Register</Link>
-            </p>
-        </>
-    )
+  return (
+    <>
+      <div className=" py-4 px-8 bg-red-100 rounded-2xl w-[30%]  mx-auto  my-20 ">
+        <h2 className="text-center my-4 text-3xl font-medium"> Login Form</h2>
+        <form action="" onSubmit={handleSubmit} className="flex-col flex">
+          <input
+            type="email"
+            placeholder="Enter Email"
+            name="email"
+            value={data.email}
+            onChange={handlechange}
+            className="border mt-4 pl-2"
+          />
+          <input
+            type="password"
+            placeholder="Enter Password"
+            name="password"
+            value={data.password}
+            onChange={handlechange}
+            className="border mt-4 pl-2"
+          />
+          <button
+            type="submit"
+            className="bg-green-300 py-1 my-8 rounded text-xl font-bold"
+          >
+            {" "}
+            Login
+          </button>
+        </form>
+      </div>
+      <p className="my-8">
+        New User ?{" "}
+        <Link to="/user/register" className="text-blue-700 ">
+          {" "}
+          Register
+        </Link>
+      </p>
+    </>
+  );
 }
 
-export default Login
+export default Login;
