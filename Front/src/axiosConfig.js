da54b63391c143b48094e5af6ae8f0ac635e1bb4
Rishijangir566 <rishijangirjr@gmail.com>
2025-04-24 15:44:25 +0530

    DEBUGGING added withCredentials in axiosConfig

--- Front/src/axiosConfig.js
+++ Front/src/axiosConfig.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    // baseURL:"http://localhost:3000/api"})
-    baseURL:"https://ecommercefullstack-back.onrender.com/api"})
-export default instance
+  // baseURL:"http://localhost:3000/api"})
+  baseURL: "https://ecommercefullstack-back.onrender.com/api",
+  withCredentials: true,
+});
+export default instance;
