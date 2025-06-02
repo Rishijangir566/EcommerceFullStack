26c5a1c5f06e42b97b776e308f966c2cd90c2769
Rishijangir566 <rishijangirjr@gmail.com>
2025-06-02 14:21:59 +0530

    coupon added new 12

--- Front/src/axiosConfig.js
+++ Front/src/axiosConfig.js
@@ -2,8 +2,6 @@ import axios from "axios";
 
 const instance = axios.create({
   // baseURL:"http://localhost:3000/api"})
-  // baseURL: "https://ecommercefullstack-back.onrender.com/api",
-  // baseURL:"http://localhost:3000/api"})
   baseURL:"https://ecommercefullstack-back.onrender.com/api"})
 
 // >>>>>>> 749400bc92bc58f6700d260c3f33df64e6283085
